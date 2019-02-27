a249ee97a16b1b10bfc0eca98036206f0722246d
bavrahami00 <bavrahami00@stuy.edu>
2019-02-27 13:50:28 -0500

    Updated format to agree with variable naming

--- Maze.java
+++ Maze.java
@@ -2,13 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Maze {
-  public static void main(String[] args) {
-    char[][] field = new char[1][1];
-    try {
-      field =
-    }
-  }
-  public static char[][] open(String file) throws FileNotFoundException{
+  private char[][] maze;
+  public char[][] open(String file) throws FileNotFoundException{
     File f = new File(file);
     Scanner s = new Scanner(f);
     int count = 0;
@@ -17,16 +12,26 @@ public class Maze {
     }
     s = new Scanner(f);
     String t = s.nextLine();
-    char[][] field = new char[count][t.length()];
+    char[][] maze = new char[count][t.length()];
     s = new Scanner(f);
     count = 0;
     while (s.hasNext()) {
       t = s.nextLine();
       for (int x = 0; x < t.length(); x++) {
-        field[count][x] = t.charAt(x);
+        maze[count][x] = t.charAt(x);
       }
       count++;
     }
-    return field;
+    return maze;
+  }
+  public void toString() {
+    String ans = "";
+    for (int i = 0; i < maze[0].length; i++) {
+      for (int x = 0; x < maze.length; x++) {
+        ans += maze[x][i];
+      }
+      ans += "\n";
+    }
+    return ans;
   }
 }
